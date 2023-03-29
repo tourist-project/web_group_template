99ef0b00935e72e9627accdb2ad2c21e6aad7c23
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2023-03-30 06:51:12 +0900

    色々揃えた

---
+++ src/lib/Firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PJ_ID,
+  storageBucket: process.env.BUCKET,
+  messagingSenderId: process.env.MESSAGE_SEND_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
